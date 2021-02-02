622a4285000aeef552a2af7c3152067b219994f6
pancenyang <pancenyang@apexsoft.com.cn>
2021-02-02 15:01:20 +0800

    简化函数步骤

--- src/tabs.vue
+++ src/tabs.vue
@@ -31,23 +31,31 @@ export default {
       eventBus: this.eventBus,
     };
   },
-  mounted() {
-    if (this.$children.length === 0) {
-      console &&
-        console.warn &&
-        console.warn(
-          "tabs的子组件应该是tabs-head,tabs-body,但是你没有写子组件！"
-        );
-    }
-    this.$children.forEach((vm) => {
-      if (vm.$options.name && vm.$options.name === "P-tabs-head") {
-        vm.$children.forEach((child) => {
-          if (child.name && child.name === this.selected) {
-            this.eventBus.$emit("update:selected", this.selected, child);
-          }
-        });
+  methods: {
+    checkChildren() {
+      if (this.$children.length === 0) {
+        console &&
+          console.warn &&
+          console.warn(
+            "tabs的子组件应该是tabs-head,tabs-body,但是你没有写子组件！"
+          );
       }
-    });
+    },
+    selectTab() {
+      this.$children.forEach((vm) => {
+        if (vm.$options.name && vm.$options.name === "P-tabs-head") {
+          vm.$children.forEach((child) => {
+            if (child.name && child.name === this.selected) {
+              this.eventBus.$emit("update:selected", this.selected, child);
+            }
+          });
+        }
+      });
+    },
+  },
+  mounted() {
+    this.checkChildren();
+    this.selectTab();
   },
 };
 </script>
